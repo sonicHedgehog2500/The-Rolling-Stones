fe09823248826e8fc018e63e266f501a4e7fb383
sonicHedgehog2500 <48524624+sonicHedgehog2500@users.noreply.github.com>
2021-11-14 17:08:56 +0000

    Add files via upload

---
+++ rollingStonesScript.js
@@ -0,0 +1,7 @@
+	
+    /*
+	 * Displays the disclaimer
+	 */
+	function viewDisclaimer(){
+		alert("This site has been constructed as part of an assessment for module 28 Website Production at the City Of Oxford College. It is not a working website and is not connected with any site or individual referenced. The views and opinions expressed within these pages are personal and should not be construed as reflecting the views and opinions of the City Of Oxford College.");
+	}
